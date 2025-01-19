5a63c6db4211c6f77e037d5affafc3377dc76021
NehaBongarde17 <nehabongarde6@gmail.com>
2025-01-19 14:29:30 +0530

    Add files via upload

---
+++ Arithmetic operation.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int a;
+    int b;
+    cout<<"Enter a & b value:";
+    cin>>a;
+    cin>>b;
+    cout << "a+b="<<a+b<<endl;
+    cout << "a-b="<<a-b<<endl;
+    cout << "a*b="<<a*b<<endl;
+    cout << "a/b="<<a/b<<endl;
+    cout << "a%b="<<a%b<<endl;
+
+
+    return 0;
+}
