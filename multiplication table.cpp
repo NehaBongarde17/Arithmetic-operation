1ae81fd44ed6911ab6aea19544282660fcaabae5
NehaBongarde17 <nehabongarde6@gmail.com>
2025-01-19 14:32:25 +0530

    Add files via upload

---
+++ multiplication table.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Write C++ code here
+    int number;
+    cout<<"Enter the number:";
+    cin>>number;
+    for(int i=1;i<=10;i++){
+        cout<<i*number<<endl;
+    }
+}
